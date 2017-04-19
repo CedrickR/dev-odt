6ed4cdafe81ae8142501f59d5e775623fdd4a5f4
cedrick <ckrom26@gmail.com>
2017-04-19 17:19:12 +0200

    test1 - docx et pdf ok

--- src/Command/CafCommand.php
+++ src/Command/CafCommand.php
@@ -21,19 +21,22 @@ class CafCommand extends Command
                 'fichiers',
                 InputArgument::REQUIRED | InputArgument::IS_ARRAY,
                 'Les noms de fichier MOF à traiter'
-                )/*
-            ->addOption(
-               'type',
-               null,
-               InputOption::VALUE_REQUIRED ,
-               'Le type de fichier à parser : LOCAL, NATIONAL'
                 )*/
+            ->addOption(
+               'test1',
+               null,
+               InputOption::VALUE_NONE ,
+               'Premier test avec un fichier word'
+                )
 
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-
+        
+        // Définition des propriétés pour générer les pdf
+        \PhpOffice\PhpWord\Settings::setPdfRendererPath('vendor/tecnickcom/tcpdf');
+        \PhpOffice\PhpWord\Settings::setPdfRendererName('TCPDF'); 
 
       
         //Affihag des données
@@ -41,6 +44,44 @@ class CafCommand extends Command
         $output->getFormatter()->setStyle('fire', $style);
         $titre = "<fire>Ceci est une commande de test</fire>";
         $output->writeln($titre);
+
+        if( $input->getOption('test1')){
+          $this->test1('helloWorld');
+        }
+
+    }
+
+    /**
+     *
+     * Test pour essayer word
+     * @var none
+     * @return none
+     */
+    private function test1($name, $rep='output')
+    {
+      // Creating the new document...
+      $phpWord = new \PhpOffice\PhpWord\PhpWord();
+      // Adding an empty Section to the document...
+      $section = $phpWord->addSection();
+      // Adding Text element to the Section having font styled by default...
+      $section->addText(
+          '"Learn from yesterday, live for today, hope for tomorrow. '
+              . 'The important thing is not to stop questioning." '
+              . '(Albert Einstein)'
+      );
+      // Saving the document as ODF file...
+      $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+      $fileName = $rep  . DIRECTORY_SEPARATOR . $name . '.docx';
+      $objWriter->save($fileName);
+
+      //Load temp file
+      $phpWord = \PhpOffice\PhpWord\IOFactory::load($fileName); 
+
+      //Save it
+      $xmlWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord , 'PDF');
+      $fileNamePDF = $rep  . DIRECTORY_SEPARATOR . $name . '.pdf';
+      $xmlWriter->save($fileNamePDF); 
+
 
     }
 
